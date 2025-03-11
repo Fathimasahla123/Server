73c36fc27f182018415e98de5a5c224161928cdc
Fathima sahla <sahlarazak234@gmail.com>
2025-03-11 16:49:52 +0400

    reservation controller and routes crated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const reservationRoutes = require("./src/routes/reservationRoutes")
 
 const connectDB = require("./src/config/db");
 connectDB();
@@ -9,6 +10,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/reservations', reservationRoutes);
+app.all("*", (req,res)=>{
+    res.send("api is working");
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- src/models/reservationModel.js
+++ src/models/reservationModel.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const reservationSchema = new mongoose.Schema({
   customerName: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
+  email: { type: String, required: true },
+  password:{ type: String, required: true },
+  phonenumber: { type: Number, required: true},
   date: { type: Date, required: true },
   time: { type: String, required: true },
   guests: { type: Number, required: true },
