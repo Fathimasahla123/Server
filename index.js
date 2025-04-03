3f5d3ec5e88a0b01fbf5857701b9b498c20b1638
Fathima sahla <sahlarazak234@gmail.com>
2025-04-03 18:40:17 +0400

    changes in index.js

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173/"],
+    origin: "http://localhost:5173/",
     Credentials: true,
-    methods: ["*"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  
   })
 );
 
