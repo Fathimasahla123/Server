5f44d8457ef49c63d8a6b29ec73caa03bb62479c
Fathima sahla <sahlarazak234@gmail.com>
2025-04-28 14:58:37 +0400

    changes in index.js

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173",
+    "https://restaurant-management-system-frontend.onrender.com"
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
